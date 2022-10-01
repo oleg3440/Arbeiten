efbd76ffd0d8655aa0dd87106528e2c1dfbe817d
yegor3452 <ntsirius@yandex.ru>
2022-10-01 09:31:43 +0500

    Example005_ConditionIfEls

---
+++ Example005_ConditionIfEls/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+
+       Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+}
