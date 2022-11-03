68f73ad58496dc30f23b705439442713fa17c5d0
yegor3452 <ntsirius@yandex.ru>
2022-11-03 19:42:35 +0500

    13.1

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -37,3 +37,39 @@ PrintArray(matrix);
 FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
+
+
+
+//Рекурсия
+int Factorial(int n)
+{
+ // 1! = 1
+ // 0! = 1
+ if(n == 1) return 1;  // если n = 1, возвращаем 1.
+ else return n * Factorial(n-1); // В противном случае берём текущее 
+ //значение и умножаем на факториал предыдущего числа (n - 1).
+}
+Console.WriteLine(Factorial(3)); // 1 * 2 * 3 = 6
+
+//или
+int Factorial(int n)
+{
+ // 1! = 1
+ // 0! = 1
+ if(n == 1) return 1;
+ else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+ Console.WriteLine(Factorial(i));
+}
+//числа Фибоначчи через рекурсию, возврат
+double Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 50; i++)
+{
+ Console.WriteLine(Fibonacci(i));
+}
